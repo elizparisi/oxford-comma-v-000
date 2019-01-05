3283be7662e8b068374dd4f14819dce94abe11f8
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-05 05:08:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,8 @@ def oxford_comma(array)
   ["red", "blue", "green"]
   if array.length == 2 
     return #{array[0] and #{array[1]}
-    [].join(",")
+  elisf array.length < 2 
+    return #{array[2]}
+end
+  array.join(" ,")
 end
