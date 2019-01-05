df6e480ae3e68248e84ec2c23cabf434267506c6
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-05 05:14:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   ["red", "blue", "green"]
   if array.length == 2 
-    return "#{array[0} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   elsif array.length > 2 
     array[-1].insert(0, "and ")
   end
